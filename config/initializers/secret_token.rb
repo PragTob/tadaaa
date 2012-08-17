d8fe517c5bfe405ad320185c624ae3130e4af444
Tobias Pfeiffer <tobi@mint>
2012-08-17 21:54:33 +0200

    Initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tadaaa::Application.config.secret_token = '111ad563fd8cbb126cb354c66170ad3b42eeb986f84ba51a63d968f2ab21212ed1b582e3cddbfae9b9a47af2be3326cd2dc2bc563e0eb30bf4b7fcdf83f5622a'
