c6269d43baa58efd5f3ff05fe5cf300d1092a0ef
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-08-18 20:51:08 +0200

    Offer belongs to User model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ActiveRecord::Base
+  belongs_to :user
   attr_accessible :asking_price, :description, :title
   
   validates :title, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120817200758) do
+ActiveRecord::Schema.define(:version => 20120818184546) do
 
   create_table "offers", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20120817200758) do
     t.integer  "asking_price"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
